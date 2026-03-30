5256f078cfacda9c1a577b1cff7b4d26c1f1366c
dirass254 <ndirangucornelius@gmail.com>
2026-03-30 10:38:20 +0300

    Fixed a bug in implementation of alertMessage

--- starter/routes/viewRouter.js
+++ starter/routes/viewRouter.js
@@ -9,10 +9,11 @@ const {
   getAccount,
   updateUserData,
   getMyTours,
+  alert,
 } = require('../controllers/viewController');
 const router = express.Router();
 // we use this middleware for all request in our website to display the alert message after the user has successfully booked a tour, because we are passing the alert query parameter in the success_url of the checkout session, so we can check for that query parameter in this middleware and then display the alert message if it exists. We use this middleware for all request because we want to display the alert message on all pages, not just the my-tours page, because after the user has successfully booked a tour, they will be redirected to the my-tours page, but they can also navigate to other pages and we still want to display the alert message on those pages.
-router.use(bookingController.alert);
+router.use(alert);
 // we do not need to pass the isLoggedIn middleware to routes that require authentication to avoid querying the user twice
 // use isLogged in for all routes that does not require authentication (e.g. overview and tour page) to check if the user is logged in and then render the appropriate header (with login/logout button)
 router.get('/', isLoggedIn, getOverview);
